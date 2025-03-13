abb064f87b67808251535c0615ade40261a0875e
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-03-14 01:12:29 +0530

    resolve minor stling isuue

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 export const Button:React.FC<ButtonProps> = ({label, onClick, icon}) => {
     return (
-        <button className="flex items-center justify-center px-4 py-2 h-9 text-white text-sm font-semibold rounded-md bg-gray-900 hover:bg-gray-800 hover:shadow-md dark:bg-white dark:text-gray-900 dark:hover:bg-white/90 text-nowrap cursor-pointer">
+        <button className="flex items-center justify-center px-4 py-2 h-9 leading-none text-white text-sm font-semibold rounded-md bg-gray-900 hover:bg-gray-800 hover:shadow-md dark:bg-white dark:text-gray-900 dark:hover:bg-white/90 text-nowrap cursor-pointer">
         {label}
         </button>
     )
