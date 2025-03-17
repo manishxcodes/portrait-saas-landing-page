a07d4954d418ed0e562ee950cd6c5d9149efcca2
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-03-17 23:15:54 +0530

    add faq section

---
+++ src/components/FAQ.tsx
@@ -0,0 +1,9 @@
+import { Banner } from "./Banner"
+
+export const FAQ = () => {
+  return (
+    <Banner 
+    title="Frequently asked questions"
+    description="Everything you need to know about our AI portrait studio." />
+  )
+}
