6d9076e222797fe8dfc2f0848115ccb78abd7f4f
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-03-23 00:37:31 +0530

    add button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
     icon?: React.ReactNode
 }
 
-export const Button:React.FC<ButtonProps> = ({label, onClick, icon}) => {
+export const Button:React.FC<ButtonProps> = ({label}) => {
     return (
         <button className="flex items-center justify-center px-4 py-2 h-9 leading-none text-white text-sm font-semibold rounded-md bg-gray-900 hover:bg-gray-800 hover:shadow-md dark:bg-white dark:text-gray-900 dark:hover:bg-white/90 text-nowrap cursor-pointer">
         {label}
