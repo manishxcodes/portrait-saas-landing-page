5596824a35cac4384a1751e7e0ae75c8d912143b
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-03-22 21:05:54 +0530

    minor style fix

--- src/components/ui/PriceCard.tsx
+++ src/components/ui/PriceCard.tsx
@@ -16,7 +16,7 @@ export const PriceCard = ({header, price, description, buttonText, feature }: Pr
         whileInView={{opacity: 1, y: 0}}
         transition={{duration: 0.3, ease: "easeOut"}}
         viewport={{once: true, amount: 0.1}}
-    className="bg-zinc-50 dark:bg-zinc-950 shadow-sm hover:shadow-md border border-black/5 dark:border-white/10 transition-all duration-100 backdrop:blur-xs px-4 pb-8 rounded-3xl w-full mb-4">
+    className="bg-zinc-[#F2F0EF] dark:bg-zinc-950 shadow-sm border border-black/5 dark:border-white/10 transition-all duration-100 backdrop:blur-xs px-4 pb-8 rounded-3xl w-full mb-4 hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] dark:hover:shadow-[0_3px_10px_rgb(256,256,256,0.4)]">
         <div className="px-4 py-2 flex justify-between mt-4 w-full">
             <h1 className="text-xl text-black dark:text-white font-semibold font-stretch-120%">{header}</h1>
         </div>
@@ -25,12 +25,12 @@ export const PriceCard = ({header, price, description, buttonText, feature }: Pr
             <span className="text-[16px] text-neutral-500">&nbsp; /month</span>
         </div>
         <div className="px-4  flex items-center w-full mb-4">
-            <p className="text-[17px] max-w-[650px] w-full md:text-[18px] text-black/60 dark:text-white/60 leading-relaxed ">
+            <p className="text-[16px] max-w-[650px] w-full md:text-[18px] text-black/60 dark:text-white/60 leading-relaxed ">
             {description}
             </p>
         </div>
         <div className="px-4  flex items-center w-full mb-4">
-            <button className="flex items-center justify-center px-4 py-4  leading-none text-black border border-black/3 dark:text-white text-sm rounded-xl bg-neutral-100  dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition-all duration-100 backdrop:blur-xs hover:shadow-sm  text-nowrap cursor-pointer w-full">
+            <button className="flex items-center justify-center px-4 py-4  leading-none text-black border border-black/3 dark:text-white text-sm rounded-xl bg-gray-100  dark:bg-neutral-800 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition-all duration-100 backdrop:blur-xs hover:shadow-sm  text-nowrap cursor-pointer w-full">
                {buttonText}
             </button>
         </div>
