f94c5c5c4fbf0472333c420485b71c748e696410
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-03-27 13:08:33 +0530

    add onclick handler

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,11 +2,13 @@ interface ButtonProps {
     label: String
     onClick?: () => void
     icon?: React.ReactNode
+    
 }
 
-export const Button:React.FC<ButtonProps> = ({label}) => {
+export const Button:React.FC<ButtonProps> = ({label, onClick}) => {
     return (
-        <button className="flex items-center justify-center px-4 py-2 h-9 leading-none text-white text-sm font-semibold rounded-md bg-gray-900 hover:bg-gray-800 hover:shadow-md dark:bg-white dark:text-gray-900 dark:hover:bg-white/90 text-nowrap cursor-pointer">
+        <button className="w-full flex items-center justify-center px-4 py-2 h-9 leading-none text-white text-sm font-semibold rounded-md bg-gray-900 hover:bg-gray-800 hover:shadow-md dark:bg-white dark:text-gray-900 dark:hover:bg-white/90 text-nowrap cursor-pointer"
+        onClick={onClick}>
         {label}
         </button>
     )
